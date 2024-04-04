e475d766e31dd6018d3065f599c2b60033e0118a
heygwangjin <heygwangjin@gmail.com>
2024-04-05 01:21:22 +0900

    chore: react-devtools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <script src="http://localhost:8097" async></script>
     </html>
   );
 }
