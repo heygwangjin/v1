33049e5cb1ffd34544a8d9f5b3e8ca83f730223b
heygwangjin <heygwangjin@gmail.com>
2024-04-12 22:17:10 +0900

    chore: Add VisuallyHidden component

---
+++ src/components/VisuallyHidden/VisuallyHidden.tsx
@@ -0,0 +1,31 @@
+// josh w comeau's VisuallyHidden component
+
+// These styles will make sure the component
+// is not visible, but will still be announced
+// by screen readers.
+//
+// Adding “display: none” would hide the
+// element from ALL users, including those
+// using screen-readers.
+import { cn } from "@/utils";
+
+interface VisuallyHiddenProps extends React.HTMLAttributes<HTMLSpanElement> {
+  as?: React.ElementType;
+  className?: string;
+  children: React.ReactNode;
+}
+
+function VisuallyHidden({
+  as: Element = "span",
+  className,
+  children,
+  ...props
+}: VisuallyHiddenProps) {
+  return (
+    <Element className={cn("sr-only", className)} {...props}>
+      {children}
+    </Element>
+  );
+}
+
+export default VisuallyHidden;
