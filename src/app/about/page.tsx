3b101e7148c3f5451cc414f9e508373c92774543
heygwangjin <heygwangjin@gmail.com>
2024-04-13 23:00:48 +0900

    feat: Add Avatar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+import Avatar from '@/components/Avatar';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,5 +7,25 @@ export const metadata: Metadata = {
 };
 
 export default function About() {
-  return <div></div>;
+  return (
+    <main className="w-full flex justify-center pt-8 sm:pt-16 lg:pt-32 pb-28 lg:pb-16 px-4 sm:px-8 xl:px-0">
+      <div className="max-w-screen-sm w-full h-full flex flex-col gap-8 justify-start items-center">
+        <div className="flex justify-between w-full">
+          <div className="flex items-center gap-4">
+            <Avatar
+              src="/me.jpg"
+              alt="gwangjin's profile picture"
+              size={80}
+              className="p-1 border border-dashed border-neutral-500"
+            />
+            <div>
+              <h1 className="text-lg font-semibold">Gwangjin Kim</h1>
+              <p className="text-neutral-500">Software Developer</p>
+            </div>
+          </div>
+          <button>Resume</button>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@/utils';
+import Image from 'next/image';
+
+interface AvatarProps {
+  src: string;
+  alt: string;
+  size?: number;
+  className?: string;
+}
+
+function Avatar({ src, alt, size = 40, className, ...props }: AvatarProps) {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={size}
+      height={size}
+      className={cn('rounded-full', className)}
+      {...props}
+    />
+  );
+}
+
+export default Avatar;
