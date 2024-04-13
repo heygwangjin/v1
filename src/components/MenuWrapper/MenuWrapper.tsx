3bb0ee80fdefaf6cf20ab2e5af20d2da4a75a63a
heygwangjin <heygwangjin@gmail.com>
2024-04-13 17:40:34 +0900

    feat: Add border to menu in dark mode

--- src/components/MenuWrapper/MenuWrapper.tsx
+++ src/components/MenuWrapper/MenuWrapper.tsx
@@ -9,7 +9,7 @@ function MenuWrapper({ children, showOnMobile = true }: MenuWrapperProps) {
   return (
     <div
       className={cn(
-        'bg-neutral-200 dark:bg-neutral-800 p-px rounded-full overflow-hidden relative shadow-sm shadow-neutral-400 dark:shadow-black z-0 shrink-0',
+        'bg-neutral-200 dark:bg-neutral-800 p-px rounded-full overflow-hidden relative shadow-sm shadow-neutral-400 dark:shadow-black z-0 shrink-0 dark:border dark:border-neutral-600',
         showOnMobile ? 'block' : 'hidden sm:block'
       )}
     >
