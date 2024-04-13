b7802526d527f7f88984806269d0c5aed1c55096
heygwangjin <heygwangjin@gmail.com>
2024-04-13 16:51:30 +0900

    feat: Add empty about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: 'More about me and my work experience.',
+};
+
+export default function About() {
+  return <div></div>;
+}
