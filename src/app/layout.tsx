ffbcdfeeb0243f4ea2ed70ebd7adc0b8a9edfd1c
heygwangjin <heygwangjin@gmail.com>
2024-09-22 16:44:44 +0900

    chore: Remove react-devtools for brave

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,9 +37,6 @@ export default function RootLayout({
         {children}
         <Analytics />
       </body>
-      {process.env.NODE_ENV === "development" && (
-        <script src="http://localhost:8097" async></script>
-      )}
     </html>
   );
 }
